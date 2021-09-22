f22affaaa31377133adba87279e18400d13eaaba
Kristina Levina <existentialantiproton@protonmail.com>
2021-09-22 16:07:58 +0200

    parameters were obtained

--- R/data_request.R
+++ R/data_request.R
@@ -1,16 +1,30 @@
-#The API 
-#The used guide https://www.dataquest.io/blog/r-api-tutorial/
+getWeather <- function(){
+  #The API 
+  #The used guide https://www.dataquest.io/blog/r-api-tutorial/
+  
+  library(httr)
+  library(jsonlite)
+  
+  #Server request:
+  request <- httr::GET(paste0("https://opendata-download-metanalys.smhi.se/api/category/mesan1g/version/2/geotype/point/lon/16/lat/58/data.json"))
+  
+  #print(request)
+  
+  #Convert from raw unicode to .json to a list
+  data <- jsonlite::fromJSON(rawToChar(request$content))
+  location <- data$geometry
+  
+  print("The chosen location:")
+  print(location)
+  
+  #Create a data.frame for analysis. 24 entries (time steps: 00:00-23.00)
+  parameters <- data$timeSeries$parameters
+  
+  print("Obtained parameters:")
+  print(parameters)
+  
+  currentDate = Sys.Date()
 
-#Source: Kolada
+}
 
-library(httr)
-library(jsonlite)
-
-#Server request:
-request <- httr::GET("https://opendata-download-metanalys.smhi.se/api/category/fwia1g/version/1/daily/geotype/point/lon/20/lat/58/data.json")
-
-print(request)
-#Convert from raw unicode to .json to a list
-data <- jsonlite::fromJSON(rawToChar(request$content))
-keys <- names(data)
-print(as.data.frame(data))
+#getWeather()
